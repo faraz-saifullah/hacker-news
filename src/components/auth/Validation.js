7c910366b3dd31b28667b3c3603a0aec025f77e2
Faraz Saifullah <fsaifullah@tekion.com>
2021-02-05 13:09:43 +0530

    mend

--- src/components/auth/Validation.js
+++
@@ -1,8 +0,0 @@
-export function validateLoginInput(loginInput) {
-  if (!loginInput.username) {
-    return 'Username can not be empty';
-  } else if (!loginInput.password) {
-    return 'Password can not be empty';
-  }
-  return '';
-}
