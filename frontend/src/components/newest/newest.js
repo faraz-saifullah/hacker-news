01d4616787a3889e9d33ee3a79bf8a9641e1bd12
Faraz Saifullah <fsaifullah@tekion.com>
2021-02-09 18:32:03 +0530

    Newest page connected to backedn

--- frontend/src/components/newest/newest.js
+++ frontend/src/components/newest/newest.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { getAllPosts } from '../../api/post';
+import Loader from '../loader/loader';
 import PostsList from '../posts/postsList';
 
 export default function Newest({ history }) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    let allPosts = JSON.parse(window.localStorage.getItem('allPosts'));
-    allPosts.sort(function (post1, post2) {
-      if (post1.postedTime > post2.postedTime) {
-        return 1;
-      } else if (post1.postedTime < post2.postedTime) {
-        return -1;
-      }
-      return 0;
-    });
-    setPosts(allPosts);
+    setIsLoading(true)
+    getAllPosts().then((allPosts) => {
+      allPosts.sort(function (post1, post2) {
+        if (post1.postedTime > post2.postedTime) {
+          return 1;
+        } else if (post1.postedTime < post2.postedTime) {
+          return -1;
+        }
+        return 0;
+      });
+      setPosts(allPosts);
+      setIsLoading(false);
+    })
   }, []);
 
-  return <PostsList history={history} postsList={posts} />;
+  return isLoading ? <Loader /> : <PostsList history={history} postsList={posts} />;
 }
